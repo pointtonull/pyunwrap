8168df69305aa732ebe349c1af7cad28ee5bb0b6
Carlos Cabrera <point_to_null@esdebian.org>
2012-02-08 01:10:43 -0300

    improve git detection

--- src/executables.py
+++ src/executables.py
@@ -33,6 +33,7 @@ def get_paths(command):
     for command in posibles:
         all_paths = (os.path.realpath(os.path.join(dirname, command))
             for dirname in dirs)
+        print all_paths
         paths += [path for path in all_paths if os.path.exists(path)]
 
     paths = [path for path in paths if path != my_path]
