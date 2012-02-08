ac926615be12c7e5e3e9a9cb2b6451a8aada0ab8
Carlos Cabrera <point_to_null@esdebian.org>
2012-02-08 18:21:21 -0300

    FMT pseudocode

---
+++ src/fmt.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+#-*- coding: UTF-8 -*-
+
+
+"""
+This scrip 
+image = [m, n]
+ft_magnitude = |fft(image)|
+lp_ftm = logpolar(ft_magnitude)
+mt = fft(lp_ftm)
+view(mt)
+"""
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    exit(main())
